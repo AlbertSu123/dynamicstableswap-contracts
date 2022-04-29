ef54eb1d98ab554764657178269de4fb7f7d72a4
Albert Su <albert_su@berkeley.edu>
2022-04-29 02:11:44 -0700

    temp stash

--- deploy/evmos_testnet/691_deploy_SimpleRewarder.ts
+++ deploy/evmos_testnet/691_deploy_SimpleRewarder.ts
@@ -3,21 +3,22 @@ import { BigNumber } from "ethers"
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { BIG_NUMBER_1E18 } from "../../test/testUtils"
+import { ZERO_ADDRESS } from "../../test/testUtils"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId, ethers } = hre
   const { deploy, execute, get, getOrNull, save, read } = deployments
   const { deployer } = await getNamedAccounts()
 
-  if ((await getOrNull("SimpleRewarder")) == null) {
-    const result = await deploy("SimpleRewarder", {
-      from: deployer,
-      log: true,
-      args: [(await get("MiniChefV2")).address],
-      skipIfAlreadyDeployed: true,
-    })
+  // if ((await getOrNull("SimpleRewarder")) == null) {
+    // const result = await deploy("SimpleRewarder", {
+    //   from: deployer,
+    //   log: true,
+    //   args: [(await get("MiniChefV2")).address],
+    //   skipIfAlreadyDeployed: true,
+    // })
 
-    await save("SimpleRewarder_SPA", result)
+    // await save("SimpleRewarder_SPA", result)
 
     //Deploy SPA token
     // Deploy token to use instead of SDL which doesn't exist on saddle
@@ -25,24 +26,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       SPA: ["SPA", "SPA", "18"],
     }
     const SPA_MINTED = 50_000_000
-    for (const token in TOKENS_ARGS) {
-      await deploy(token, {
-        from: deployer,
-        log: true,
-        contract: "GenericERC20",
-        args: TOKENS_ARGS[token],
-        skipIfAlreadyDeployed: true,
-      })
-      // If it's on hardhat, mint test tokens
-      const decimals = TOKENS_ARGS[token][2]
-      await execute(
-        token,
-        { from: deployer, log: true },
-        "mint",
-        deployer,
-        BigNumber.from(10).pow(decimals).mul(SPA_MINTED),
-      )
-    }
+    // for (const token in TOKENS_ARGS) {
+    //   await deploy(token, {
+    //     from: deployer,
+    //     log: true,
+    //     contract: "GenericERC20",
+    //     args: TOKENS_ARGS[token],
+    //     skipIfAlreadyDeployed: true,
+    //   })
+    //   // If it's on hardhat, mint test tokens
+    //   const decimals = TOKENS_ARGS[token][2]
+    //   await execute(
+    //     token,
+    //     { from: deployer, log: true },
+    //     "mint",
+    //     deployer,
+    //     BigNumber.from(10).pow(decimals).mul(SPA_MINTED),
+    //   )
+    // }
 
     const PID = 2
     const lpToken = (await get("SaddleArbUSDSMetaPoolLPToken")).address
@@ -52,45 +53,45 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       BigNumber.from(SPA_MINTED).div(10),
     )
     const rewardPerSecond = TOTAL_LM_REWARDS.div(6 * 4 * 7 * 24 * 3600) // SPA reward per second
-    await execute(
-      "SPA",
-      { from: deployer, log: true },
-      "approve",
-      (
-        await get("SimpleRewarder_SPA")
-      ).address,
-      TOTAL_LM_REWARDS,
-    )
-    await execute(
-      "SPA",
-      { from: deployer, log: true },
-      "transfer",
-      (
-        await get("SimpleRewarder_SPA")
-      ).address,
-      TOTAL_LM_REWARDS,
-    )
-    await execute(
-      "SPA",
-      { from: deployer, log: true },
-      "approve",
-      (
-        await get("MiniChefV2")
-      ).address,
-      TOTAL_LM_REWARDS,
-    )
-    await execute(
-      "SPA",
-      { from: deployer, log: true },
-      "transfer",
-      (
-        await get("MiniChefV2")
-      ).address,
-      TOTAL_LM_REWARDS,
-    )
+    // await execute(
+    //   "SPA",
+    //   { from: deployer, log: true },
+    //   "approve",
+    //   (
+    //     await get("SimpleRewarder_SPA")
+    //   ).address,
+    //   TOTAL_LM_REWARDS,
+    // )
+    // await execute(
+    //   "SPA",
+    //   { from: deployer, log: true },
+    //   "transfer",
+    //   (
+    //     await get("SimpleRewarder_SPA")
+    //   ).address,
+    //   TOTAL_LM_REWARDS,
+    // )
+    // await execute(
+    //   "SPA",
+    //   { from: deployer, log: true },
+    //   "approve",
+    //   (
+    //     await get("MiniChefV2")
+    //   ).address,
+    //   TOTAL_LM_REWARDS,
+    // )
+    // await execute(
+    //   "SPA",
+    //   { from: deployer, log: true },
+    //   "transfer",
+    //   (
+    //     await get("MiniChefV2")
+    //   ).address,
+    //   TOTAL_LM_REWARDS,
+    // )
 
     // Ensure pid is correct
-    expect(await read("MiniChefV2", "lpToken", PID)).to.eq(lpToken)
+    // expect(await read("MiniChefV2", "lpToken", PID)).to.eq(lpToken)
 
     // (IERC20 rewardToken, address owner, uint256 rewardPerSecond, IERC20 masterLpToken, uint256 pid)
     const data = ethers.utils.defaultAbiCoder.encode(
@@ -104,13 +105,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       ],
     )
 
+    // await execute(
+    //   "SimpleRewarder_SPA",
+    //   { from: deployer, log: true },
+    //   "init",
+    //   data,
+    // )
+
     await execute(
-      "SimpleRewarder_SPA",
+      "MiniChefV2",
       { from: deployer, log: true },
-      "init",
-      data,
+      "add",
+      1,
+      lpToken,
+      ZERO_ADDRESS,
     )
-  }
+  // }
 }
 export default func
 func.tags = ["SimpleRewarder"]

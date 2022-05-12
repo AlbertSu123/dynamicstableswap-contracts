9114d65c9c252e65a5c38b19367480d42e362f13
Albert Su <albert_su@berkeley.edu>
2022-05-12 12:16:52 -0700

    New deployment for kusd metapools on evmos testnet

--- deploy/evmos_testnet/130_deploy_MetaSwapUtils.ts
+++ deploy/evmos_testnet/130_deploy_MetaSwapUtils.ts
@@ -1,25 +1,16 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
-import { isTestNetwork } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get } = deployments
   const { libraryDeployer } = await getNamedAccounts()
 
-  if (isTestNetwork(await getChainId())) {
-    await deploy("SwapUtilsV1", {
-      from: libraryDeployer,
-      log: true,
-      skipIfAlreadyDeployed: true,
-    })
-  }
-
-  await deploy("SwapUtils", {
+  await deploy("MetaSwapUtils", {
     from: libraryDeployer,
     log: true,
     skipIfAlreadyDeployed: true,
   })
 }
 export default func
-func.tags = ["SwapUtils"]
+func.tags = ["MetaSwapUtils"]

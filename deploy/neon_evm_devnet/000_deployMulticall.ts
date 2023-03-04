9a629224f64ab5af03f5510229741e8bf3b72099
a <a@gmail.com>
2023-03-04 13:07:56 -0700

    deploy multicall to networks, ugh

--- deploy/neon_evm_devnet/000_deployMulticall.ts
+++ deploy/neon_evm_devnet/000_deployMulticall.ts
@@ -3,19 +3,14 @@ import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
-  const { deploy, get } = deployments
+  const { deploy } = deployments
   const { libraryDeployer } = await getNamedAccounts()
 
-  await deploy("SwapFlashLoan", {
+  await deploy("Multicall3", {
     from: libraryDeployer,
     log: true,
-    libraries: {
-      SwapUtils: (await get("SwapUtils")).address,
-      AmplificationUtils: (await get("AmplificationUtils")).address,
-    },
     skipIfAlreadyDeployed: true,
   })
 }
 export default func
-func.tags = ["SwapFlashLoan"]
-func.dependencies = ["AmplificationUtils", "SwapUtils"]
+func.tags = ["Multicall"]
